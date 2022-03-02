94b98af9ded532282896208f9f966977af517d54
Angelk90 <angelo.k90@hotmail.it>
2022-03-02 19:06:21 +0100

    Update action.js

--- action.js
+++ action.js
@@ -1,1 +1,8 @@
+async function getData(){
+ const url = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Meet_Truffle%21.jpg/440px-Meet_Truffle%21.jpg";
+ return url;
+}
 
+getData().then((url) => {
+  return url;
+});
