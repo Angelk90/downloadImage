f15974c0ee36a6da177795ac377b30fd673d103b
Angelk90 <angelo.k90@hotmail.it>
2022-03-04 11:59:08 +0100

    Update actionMultifile.js

--- actionMultifile.js
+++ actionMultifile.js
@@ -5,7 +5,7 @@ let c = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/T._rex_infect
 let d = ["https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/T._rex_infection.png/440px-T._rex_infection.png", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mosasaurus-Skelett.jpg/440px-Mosasaurus-Skelett.jpg"]
 let e = "['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Tyrannosaurus_rex_Sue_at_FMNH.jpg/440px-Tyrannosaurus_rex_Sue_at_FMNH.jpg']"
 let f = "['https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Tyrannosaurus_rex_Sue_at_FMNH.jpg/440px-Tyrannosaurus_rex_Sue_at_FMNH.jpg','https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Mosasaurus_beaugei_34.JPG/440px-Mosasaurus_beaugei_34.JPG']"
-return e;
+return f;
 }
 
 getData().then((url) => {
